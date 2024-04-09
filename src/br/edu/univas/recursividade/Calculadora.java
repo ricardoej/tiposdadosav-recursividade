75b09b96d399093680efde5890e1aec121114041
Laboratorio Unidade Fatima <labunivas@univas.edu.br>
2024-04-08 21:46:36 -0300

    Joao Davi Muroni Tenorio

---
+++ src/br/edu/univas/recursividade/Calculadora.java
@@ -0,0 +1,26 @@
+package br.edu.univas.recursividade;
+
+public class Calculadora {
+//Método utilizando recursividade
+    public int getFatorial(int num) {
+        if (num == 0 || num == 1) {
+            return 1;
+        } else {
+            return num * getFatorial(num - 1);
+        }
+    }
+}
+
+//Método sem utilizar recursividade
+//public int getFatorial(int num){
+//Inicializa o resultado como 1, pois o fatorial de 0 e 1 é 1
+//int resultado = 1;
+//Itera de 1 até o numero fornecido
+//for (int = 1; i <= num; i++){
+//Multiplica o resultado pelo número atual da iteração
+//resultado *= i;
+//}
+//Retorna o resultado final do fatorial
+//return resultado;
+//}
+//}
