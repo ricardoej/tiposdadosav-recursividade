7ea61288d6b5f899f0c3b81be8302e2c4177dd1d
Laboratorio Unidade Fatima <labunivas@univas.edu.br>
2024-04-08 21:48:51 -0300

    Victor Daniel Vieira da Silva

---
+++ src/br/edu/univas/recursividade/Calculadora.java
@@ -0,0 +1,38 @@
+package br.edu.univas.recursividade;
+
+public class Calculadora {
+	public int getFatorialRecursivo(int n) {
+		if (n == 0 ||n == 1) {
+	return 1;
+		}else {
+			
+			return n * getFatorialRecursivo(n - 1);
+	
+		
+		}
+	}
+
+		public int getFatorial(int n) {
+			if ( n < 0) {
+			}
+			int fatorial = 1;
+			for ( int i = 1; i <= n; i++){
+			fatorial *=i;
+			
+			}
+			return fatorial;
+			
+			
+			}
+	}
+
+
+
+
+
+
+	
+		
+
+
+
