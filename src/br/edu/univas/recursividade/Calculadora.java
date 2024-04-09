2256f4bb385da55e1ed2ca68e86b5c63a708ba6c
Your Name <you@example.com>
2024-04-08 21:50:35 -0300

    Pettrius

--- src/br/edu/univas/recursividade/Calculadora.java
+++ src/br/edu/univas/recursividade/Calculadora.java
@@ -2,14 +2,28 @@ package br.edu.univas.recursividade;
 
 public class Calculadora {
 	
-	public int getFatorial(int num) {
+	public int getFatorial(int n){
 		
-		if (num == 0) {
+		// FATORIAL SEM RECURSIVIDADE
+		/*int resultado1 = 1;
+		
+		for (int i = 1; i <= n; i++) {
+			resultado1 = resultado1 * i;
+		}
+		
+		return resultado1; */
+		
+		// Fatorial com recursividade
+		if (n < 0) {
+			 throw new IllegalArgumentException("O fatorial não pode ser negativo.");
+		}
+		
+		if (n == 0) {
 			return 1;
 		}
 		
 		else {
-			return num * getFatorial(num - 1);
+			return n * getFatorial (n - 1);
 		}
 	}
 
